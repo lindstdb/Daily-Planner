57ffed9cbe445d39ca7345ce0938968cc2866d94
lindstdb <115056565+lindstdb@users.noreply.github.com>
2023-01-03 10:24:36 -0600

    fixes the colors by re-numbering

--- script.js
+++ script.js
@@ -47,7 +47,32 @@ setInterval(function(){
   setTimeColor();
 }, 15000);
 
+// Retrieves user input from local storage and displays it in the correct time slot.
+
 var hour9 = localStorage.getItem("hour-9");
 $("#hour-9 .description").val(hour9);
 
+var hour10 = localStorage.getItem("hour-10");
+$("#hour-10 .description").val(hour10);
+
+var hour11 = localStorage.getItem("hour-11");
+$("#hour-11 .description").val(hour11);
+
+var hour12 = localStorage.getItem("hour-12");
+$("#hour-12 .description").val(hour12);
+
+var hour13 = localStorage.getItem("hour-13");
+$("#hour-13 .description").val(hour13);
+
+var hour14 = localStorage.getItem("hour-14");
+$("#hour-14 .description").val(hour14);
+
+var hour15 = localStorage.getItem("hour-15");
+$("#hour-15 .description").val(hour15);
+
+var hour16 = localStorage.getItem("hour-16");
+$("#hour-16 .description").val(hour16);
+
+var hour17 = localStorage.getItem("hour-17");
+$("#hour-17 .description").val(hour17);
 
